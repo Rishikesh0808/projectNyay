02c02c94c49d4626a235d4704041891577efcc3a
Rishikesh0808 <rishikeshreddy.arra@gmail.com>
2024-11-28 20:23:22 +0530

    added new

--- backend/app.js
+++ backend/app.js
@@ -34,12 +34,12 @@ app.get('/api', (req, res) => {
 
 // Connect to the database
 
-app.use(express.static(path.join(__dirname, '../reactproj/build/static')));
+/*app.use(express.static(path.join(__dirname, '../reactproj/build/static')));
 
 // Serve the React app for all non-API routes
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../reactproj/build', 'index.html'));
-});
+});*/
 
 
 // Middleware
